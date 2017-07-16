baae70eed40a6416194067ee1f02edc6519fa2a3
m-kouda <makotokoda82@gmail.com>
2017-07-16 15:00:21 +0900

    判定処理の追加

--- js/main.js
+++ js/main.js
@@ -13,6 +13,7 @@
   var pannel_2_timer;
   var stop_count = 0;
   var isStarted = false;
+  var results = [];
 
   var displayElements = [
     '☆', '♪', '○', '□', '△', '♡', '7'
@@ -20,29 +21,42 @@
 
   function checkResult() {
     spinBtn.className = "";
+    if (results[0] !== results[1] && results[0] !== results[2]) {
+      pannel_0_ele.className = 'pannel unmatched';
+    }
+    if (results[1] !== results[0] && results[1] !== results[2]) {
+      pannel_1_ele.className = 'pannel unmatched';
+    }
+    if (results[2] !== results[1] && results[2] !== results[0]) {
+      pannel_2_ele.className = 'pannel unmatched';
+    }
+    init();
   }
 
   // STOPボタンの処理
   btn_0_ele.addEventListener('click', function() {
     clearInterval(pannel_0_timer);
-    btn_0_ele.className = "btn inactive";
+    this.className = "btn inactive";
     stop_count++;
+    results[0] = pannel_0_ele.innerText;
     if (stop_count == 3) {
       checkResult();
     }
   });
   btn_1_ele.addEventListener('click', function() {
     clearInterval(pannel_1_timer);
-    btn_1_ele.className = "btn inactive";
+    this.className = "btn inactive";
     stop_count++;
+    results[1] = pannel_1_ele.innerText;
     if (stop_count == 3) {
       checkResult();
     }
   });
   btn_2_ele.addEventListener('click', function() {
     clearInterval(pannel_2_timer);
-    btn_2_ele.className = "btn inactive"
+    this.className = "btn inactive"
     stop_count++;
+    results[2] = pannel_2_ele.innerText;
     if (stop_count == 3) {
       checkResult();
     }
