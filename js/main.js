fb6e7160a746a5f6245f8f7c244b074812233026
m-kouda <makotokoda82@gmail.com>
2017-07-16 15:18:18 +0900

    ボタンの多重起動防止と初期化処理の追加

--- js/main.js
+++ js/main.js
@@ -11,13 +11,30 @@
   var pannel_0_timer;
   var pannel_1_timer;
   var pannel_2_timer;
-  var stop_count = 0;
-  var isStarted = false;
-  var results = [];
+  var stop_count;
+  var isStarted;
+  var results;
+  var isBtn0Clicked;
+  var isBtn1Clicked;
+  var isBtn2Clicked;
 
   var displayElements = [
     '☆', '♪', '○', '□', '△', '♡', '7'
   ];
+
+  function init() {
+    isStarted = false;
+    results = [];
+    stop_count = 0;
+    isBtn0Clicked = false;
+    isBtn1Clicked = false;
+    isBtn2Clicked = false;
+    pannel_0_timer = '';
+    pannel_1_timer = '';
+    pannel_2_timer = '';
+  }
+
+  init();
 
   function checkResult() {
     spinBtn.className = "";
@@ -30,35 +47,51 @@
     if (results[2] !== results[1] && results[2] !== results[0]) {
       pannel_2_ele.className = 'pannel unmatched';
     }
+
     init();
   }
 
   // STOPボタンの処理
   btn_0_ele.addEventListener('click', function() {
-    clearInterval(pannel_0_timer);
-    this.className = "btn inactive";
-    stop_count++;
-    results[0] = pannel_0_ele.innerText;
-    if (stop_count == 3) {
-      checkResult();
+    if (isStarted == true && isBtn0Clicked == false) {
+      isBtn0Clicked = true;
+      clearInterval(pannel_0_timer);
+      this.className = "btn inactive";
+      stop_count++;
+      results[0] = pannel_0_ele.innerText;
+      if (stop_count == 3) {
+        checkResult();
+      }
+    } else {
+      return;
     }
   });
   btn_1_ele.addEventListener('click', function() {
-    clearInterval(pannel_1_timer);
-    this.className = "btn inactive";
-    stop_count++;
-    results[1] = pannel_1_ele.innerText;
-    if (stop_count == 3) {
-      checkResult();
+    if (isStarted == true && isBtn1Clicked == false) {
+      isBtn1Clicked = true;
+      clearInterval(pannel_1_timer);
+      this.className = "btn inactive";
+      stop_count++;
+      results[1] = pannel_1_ele.innerText;
+      if (stop_count == 3) {
+        checkResult();
+      }
+    } else {
+      return;
     }
   });
   btn_2_ele.addEventListener('click', function() {
-    clearInterval(pannel_2_timer);
-    this.className = "btn inactive"
-    stop_count++;
-    results[2] = pannel_2_ele.innerText;
-    if (stop_count == 3) {
-      checkResult();
+    if (isStarted == true && isBtn2Clicked == false) {
+      isBtn2Clicked = true;
+      clearInterval(pannel_2_timer);
+      this.className = "btn inactive"
+      stop_count++;
+      results[2] = pannel_2_ele.innerText;
+      if (stop_count == 3) {
+        checkResult();
+      }
+    } else {
+      return;
     }
   });
 
@@ -67,6 +100,9 @@
     // パネルが回る処理
     if (!isStarted) {
       isStarted = true;
+      pannel_0_ele.className = "pannel";
+      pannel_1_ele.className = "pannel";
+      pannel_2_ele.className = "pannel";
       spinBtn.className = 'inactive';
       btn_0_ele.className = 'btn';
       btn_1_ele.className = 'btn';
